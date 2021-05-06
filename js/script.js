abf77e85e4887047a7182570f1476a54418581f2
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-06 17:11:42 +0200

    Tentativo fallito di inserire un countdown

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ var playerNumbers = [];
 var randomEnd = 1000;
 // tempo di attesa in secondi
 var waitingTime = 2;
+var score = 0;
+var correctList = [];
 
 // Funzioni
 // funzione per generare numeri casuali
@@ -35,6 +37,37 @@ function milliSecond(time) {
     return time * 1000;
 }
 
+// funzione di GIOCO: inserimento numeri da parte dell'utente
+function getUserNumbers() {
+    for (var i = 0; i < numberList; i++) {
+        var num = parseInt(prompt("Inserisci qui un numero che ti ricordi di quelli visti poco fa:"));
+        playerNumbers.push(num);
+
+        // verifica dei numeri e aumento punteggio
+        if (arrayPresence(num, numbersToGuess)) {
+            correctList.push(num);
+            score++;
+        }
+    }
+}
+
+// Funzione che espone i risultati finali
+function result() {
+    switch (score) {
+        case  numberList:
+            alert("Complimenti, hai indovinato tutti i numeri!\nHai una memoria di ferro!");
+        break;
+
+        case 0:
+            alert("Oh no, non hai indovinato nessun numero!\nI numeri apparsi all'inizio erano questi: " + numbersToGuess.toString().replace(/,/g, ", ") + ".\nRiprova!");
+        break;
+
+        default:
+            alert("Complimenti, hai indovinato: " + score + " numeri su " + numberList.toString().replace(/,/g, ", ") + "!\nI numeri che hai indovinato sono: " + correctList.toString().replace(/,/g, ", ") + ".\nPuoi fare di meglio! Riprova!");
+            break;
+    }
+}
+
 // Generazione dei numeri da visualizzare
 while (numbersToGuess.length < numberList) {
     var number = randomNumber(1, randomEnd);
@@ -45,46 +78,29 @@ while (numbersToGuess.length < numberList) {
 }
 console.log("Lista di numeri da ricordare: ", numbersToGuess);
 
-
 // Alert espone 5 numeri casuali
 alert("Ciao!\nPremi invio per far apparire " + numberList + " numeri.\nSe vuoi vincere, ricordali tutti!\nVIA!");
 
 alert("Questi sono i tuoi numeri:\n" + numbersToGuess.toString().replace(/,/g, ", ") + "\nProva a ricordarli tutti!\nPremi invio e hai " + waitingTime + " secondi prima di poter testare la tua memoria!");
 
+document.getElementById("countdown").innerHTML = "<p>" + waitingTime + "</p>"
+
+// Countdown 
+var countDown = setInterval( function() {
+    
+    if (waitingTime == -1) {
+        clearInterval(countDown);
+        document.getElementById("countdown").innerHTML = "<p>Via!</p>"
+    } else {
+        waitingTime--;
+    }
+}, milliSecond(waitingTime));
 
 //Gioco
-var score = 0;
-var correctList = [];
-var attempts = setTimeout( function () {
+var attempts = setTimeout( function() {
+    // inserimento numeri da parte dell'utente
+    getUserNumbers();
+    // risultato finale
+    result();
 
-    for (var i = 0; i < numberList; i++) {
-        var num = parseInt(prompt("Inserisci qui un numero che ti ricordi di quelli visti poco fa:"));
-        playerNumbers.push(num);
-
-        // verifica dei numeri e aumento punteggio
-        if (arrayPresence(num, numbersToGuess)) {
-            correctList.push(num);
-            score++;
-        }
-
-    }
-    console.log("I numeri inseriti dall'utente sono: ", playerNumbers);
-    console.log("I numeri indovinati sono: ", correctList);
-    console.log("Il tuo punteggio è: ", score);
 }, milliSecond(waitingTime));
-clearTimeout(attempts);
-
-
-// risultato finale
-switch (score) {
-    case  numberList:
-        alert("Complimenti, hai indovinato tutti i numeri!\nHai una memoria di ferro!");
-    break;
-
-    case 0:
-        alert("Oh no, non hai indovinato nessun numero!\nI numeri apparsi all'inizio erano questi: " + numbersToGuess.toString().replace(/,/g, ", ") + "\nRiprova!")
-
-    default:
-        alert("Hai indovinato: " + score + " numeri su " + numberList.toString().replace(/,/g, ", ") + "\nI numeri che hai indovinato sono: " + correctList.toString().replace(/,/g, ", ") + "\n")
-        break;
-}
